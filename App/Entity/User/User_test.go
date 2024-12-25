71ef980a194a3042f98c6255668453935a820cdc
Witchakorn Boonprakom <witchakorn.b@kkumail.com>
2024-12-26 00:50:59 +0700

    Enhance User entity with additional fields and constraints; update tests accordingly

--- App/Entity/User/User_test.go
+++ App/Entity/User/User_test.go
@@ -1,35 +1,47 @@
-package user
+package user_test
 
 import (
+	"github.com/witchakornb/student-management-system/Entity/User"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
+	"gorm.io/gorm"
 )
 
-func TestUser(t *testing.T) {
-	user := User{
+func TestUserStruct(t *testing.T) {
+	now := time.Now()
+	deletedAt := gorm.DeletedAt{Time: now, Valid: true}
+
+	u := user.User{
 		UserID:      "123",
 		Username:    "testuser",
 		Password:    "password",
-		Role:        "admin",
-		FirstNameTH: "ชื่อ",
-		LastNameTH:  "นามสกุล",
-		FirstNameEN: "FirstName",
-		LastNameEN:  "LastName",
+		Role:        "student",
+		FirstNameTH: "Test",
+		LastNameTH:  "User",
+		FirstNameEN: "Test",
+		LastNameEN:  "User",
 		Email:       "testuser@example.com",
 		Phone:       "1234567890",
-		Department:  "IT",
+		Department:  "Computer Science",
+		CreatedAt:   now,
+		UpdatedAt:   now,
+		DeletedAt:   deletedAt,
 	}
 
-	assert.Equal(t, "123", user.UserID)
-	assert.Equal(t, "testuser", user.Username)
-	assert.Equal(t, "password", user.Password)
-	assert.Equal(t, "admin", user.Role)
-	assert.Equal(t, "ชื่อ", user.FirstNameTH)
-	assert.Equal(t, "นามสกุล", user.LastNameTH)
-	assert.Equal(t, "FirstName", user.FirstNameEN)
-	assert.Equal(t, "LastName", user.LastNameEN)
-	assert.Equal(t, "testuser@example.com", user.Email)
-	assert.Equal(t, "1234567890", user.Phone)
-	assert.Equal(t, "IT", user.Department)
+	assert.Equal(t, "123", u.UserID)
+	assert.Equal(t, "testuser", u.Username)
+	assert.Equal(t, "password", u.Password)
+	assert.Equal(t, "student", u.Role)
+	assert.Equal(t, "Test", u.FirstNameTH)
+	assert.Equal(t, "User", u.LastNameTH)
+	assert.Equal(t, "Test", u.FirstNameEN)
+	assert.Equal(t, "User", u.LastNameEN)
+	assert.Equal(t, "testuser@example.com", u.Email)
+	assert.Equal(t, "1234567890", u.Phone)
+	assert.Equal(t, "Computer Science", u.Department)
+	assert.Equal(t, now, u.CreatedAt)
+	assert.Equal(t, now, u.UpdatedAt)
+	assert.Equal(t, deletedAt, u.DeletedAt)
 }
